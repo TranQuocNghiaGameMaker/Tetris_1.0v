438b4beb7afd51b9ee53178290b9aadcd261788b
Trần Quốc Nghĩa 21011618 <garenpc2003@gmail.com>
2023-01-17 12:47:21 +0700

    Fix Board

--- Assets/Scripts/Board.cs
+++ Assets/Scripts/Board.cs
@@ -6,15 +6,15 @@ public class Board : MonoBehaviour
     public Tilemap tilemap { get; private set; }
     public Piece activePiece { get; private set; }
 
-    public TetrominoData[] tetrominoes;
-    public Vector2Int boardSize = new Vector2Int(10, 20);
-    public Vector3Int spawnPosition = new Vector3Int(-1, 8, 0);
+    [SerializeField] TetrominoData[] tetrominoes;
+    [SerializeField] Vector2Int boardSize = new(10, 20);
+    [SerializeField] Vector3Int _spawnPosition = new(-1, 8, 0);
 
     public RectInt Bounds
     {
         get
         {
-            Vector2Int position = new Vector2Int(-boardSize.x / 2, -boardSize.y / 2);
+            Vector2Int position = new(-boardSize.x / 2, -boardSize.y / 2);
             return new RectInt(position, boardSize);
         }
     }
@@ -40,9 +40,9 @@ public class Board : MonoBehaviour
         int random = Random.Range(0, tetrominoes.Length);
         TetrominoData data = tetrominoes[random];
 
-        activePiece.Initialize(this, spawnPosition, data);
+        activePiece.Initialize(this, _spawnPosition, data);
 
-        if (IsValidPosition(activePiece, spawnPosition))
+        if (IsValidPosition(activePiece, _spawnPosition))
         {
             Set(activePiece);
         }
@@ -101,71 +101,4 @@ public class Board : MonoBehaviour
 
         return true;
     }
-
-    public void ClearLines()
-    {
-        RectInt bounds = Bounds;
-        int row = bounds.yMin;
-
-        // Clear from bottom to top
-        while (row < bounds.yMax)
-        {
-            // Only advance to the next row if the current is not cleared
-            // because the tiles above will fall down when a row is cleared
-            if (IsLineFull(row))
-            {
-                LineClear(row);
-            }
-            else
-            {
-                row++;
-            }
-        }
-    }
-
-    public bool IsLineFull(int row)
-    {
-        RectInt bounds = Bounds;
-
-        for (int col = bounds.xMin; col < bounds.xMax; col++)
-        {
-            Vector3Int position = new Vector3Int(col, row, 0);
-
-            // The line is not full if a tile is missing
-            if (!tilemap.HasTile(position))
-            {
-                return false;
-            }
-        }
-
-        return true;
-    }
-
-    public void LineClear(int row)
-    {
-        RectInt bounds = Bounds;
-
-        // Clear all tiles in the row
-        for (int col = bounds.xMin; col < bounds.xMax; col++)
-        {
-            Vector3Int position = new Vector3Int(col, row, 0);
-            tilemap.SetTile(position, null);
-        }
-
-        // Shift every row above down one
-        while (row < bounds.yMax)
-        {
-            for (int col = bounds.xMin; col < bounds.xMax; col++)
-            {
-                Vector3Int position = new Vector3Int(col, row + 1, 0);
-                TileBase above = tilemap.GetTile(position);
-
-                position = new Vector3Int(col, row, 0);
-                tilemap.SetTile(position, above);
-            }
-
-            row++;
-        }
-    }
-
 }
